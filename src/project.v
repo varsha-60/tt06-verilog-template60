7af0a135a47a42befa4fc59f0e1bbd3520e446b3
varsha-60 <163625331+varsha-60@users.noreply.github.com>
2024-03-16 12:33:09 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_ha (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,8 +17,11 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-endmodule
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+   assign ui_in[7:2]=6'b000000;
+   assign uo_out[7:2]=6'b000000;
+   assign uio_out = 0;
+   assign uio_oe  = 0;
+   assign uo_out[0] = ui_in[0] ^ ui_in[1];
+   assign uo_out[1] = ui_in[0] ^ ui_in[1];
+   endmodule
